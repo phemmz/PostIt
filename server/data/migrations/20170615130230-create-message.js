e229cc92b329d7951ca8be6344812824e678f119
Adetunji Femi <phemmzmcllroy@gmail.com>
2017-06-21 14:44:31 +0100

    priority column added to db

--- server/data/migrations/20170615130230-create-message.js
+++ server/data/migrations/20170615130230-create-message.js
@@ -15,6 +15,10 @@ module.exports = {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
       },
+      priority: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
