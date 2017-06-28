12a437853f2a82e8d5f281adc425e91aafe080fc
Adetunji Femi <phemmzmcllroy@gmail.com>
2017-06-28 14:46:36 +0100

    add env port

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 
 var _express = require('express');
@@ -28,11 +28,11 @@ var _dotenv = require('dotenv');
 
 var _dotenv2 = _interopRequireDefault(_dotenv);
 
-var _apiRoutes = require('./server/routes/apiRoutes.js');
+var _apiRoutes = require('./server/routes/apiRoutes');
 
 var _apiRoutes2 = _interopRequireDefault(_apiRoutes);
 
-var _index = require('./server/routes/index.js');
+var _index = require('./server/routes/index');
 
 var _index2 = _interopRequireDefault(_index);
 
@@ -40,42 +40,43 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 _dotenv2.default.config();
 
-//Express app setup
+// Express app setup
 var app = (0, _express2.default)();
-//view engine setup
-app.set('views', _path2.default.join(__dirname, "views"));
+// view engine setup
+app.set('views', _path2.default.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
-//Morgan helps log all requests to the console
+// Morgan helps log all requests to the console
 app.use((0, _morgan2.default)('dev'));
 
-//Parse incoming requests data
+// Parse incoming requests data
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 app.use(_express2.default.static(_path2.default.join(__dirname, './client/public')));
 
 app.use((0, _expressSession2.default)({
-	secret: "hello",
-	resave: false,
-	saveUninitialized: true
+  secret: 'hello',
+  resave: false,
+  saveUninitialized: true
 }));
 
-//Require our routes into the application
+// Require our routes into the application
 
 (0, _apiRoutes2.default)(app);
 (0, _index2.default)(app);
 
-//Setup a default catch-all route that sends back a welcome message in JSON format
+// Setup a default catch-all route that sends back a welcome message in JSON format
 app.get('*', function (req, res) {
-	return res.status(200).send({
-		message: 'Welcome!!!'
-	});
+  return res.status(200).send({
+    message: 'Welcome!!!'
+  });
 });
 
-var port = 8000;
+var port = parseInt(process.env.PORT, 10) || 8000;
+
 if (!module.parent) {
-	app.listen(port, function () {
-		console.log('Listening on port 8000...');
-	});
+  app.listen(port, function () {
+    // console.log('Listening on port 8000...');
+  });
 }
 
 exports.default = app;
