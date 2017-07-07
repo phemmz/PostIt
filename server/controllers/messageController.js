318e3e081e5a1b8dccdbb49cfd18f43881321812
phemmz <phemmzmcllroy@gmail.com>
2017-07-07 12:45:51 +0100

    update readme file

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,8 @@
 const Message = require('../data/models').Message;
 
-
+/**
+ * 
+ */
 export default class MessageCtrl {
   /**
    * 
@@ -53,60 +55,60 @@ export default class MessageCtrl {
   }
 }
 // exports.create = function (req, res) {
-  // console.log(req.body);
-  // console.log(req.params.groupId);
-  // if(req.session.name) {
-  // return Message
-  //   .create({
-  //     content: req.body.content,
-  //     readcheck: req.body.readcheck,
-  //     priority: req.body.priority,
-  //     groupId: req.params.groupId,
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //     res.json({
-  //       confirmation: 'fail',
-  //       message: error
-  //     });
-  //   })
-  //   .then((message) => {
-  //     res.json({
-  //       confirmation: 'success',
-  //       result: message
-  //     });
-  //   });
+// console.log(req.body);
+// console.log(req.params.groupId);
+// if(req.session.name) {
+// return Message
+//   .create({
+//     content: req.body.content,
+//     readcheck: req.body.readcheck,
+//     priority: req.body.priority,
+//     groupId: req.params.groupId,
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     res.json({
+//       confirmation: 'fail',
+//       message: error
+//     });
+//   })
+//   .then((message) => {
+//     res.json({
+//       confirmation: 'success',
+//       result: message
+//     });
+//   });
 
-  // }
-  // else {
-  //  res.json({
-  //   message: "Login to send message"
-  //  });
-  // }
+// }
+// else {
+//  res.json({
+//   message: "Login to send message"
+//  });
+// }
 // };
 
-exports.retrieve = function (req, res) {
-  // if(req.session.name) {
-  return Message.findAll({
-    where: { groupId: req.params.groupId }
-  })
-    .then((messages) => {
-      let msg = JSON.stringify(messages);
-      msg = JSON.parse(msg);
-      res.json(msg);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.json({
-        confirmation: 'fail',
-        message: error
-      });
-    });
+// exports.retrieve = function (req, res) {
+//   // if(req.session.name) {
+//   return Message.findAll({
+//     where: { groupId: req.params.groupId }
+//   })
+//     .then((messages) => {
+//       let msg = JSON.stringify(messages);
+//       msg = JSON.parse(msg);
+//       res.json(msg);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       res.json({
+//         confirmation: 'fail',
+//         message: error
+//       });
+//     });
 
-  // }
-  // else {
-  //  res.json({
-  //    message: "Login to get messages"
-  //  });
-  // }
-};
+// }
+// else {
+//  res.json({
+//    message: "Login to get messages"
+//  });
+// }
+// };
