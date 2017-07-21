29e0d22b553b75e5eb55eeac5cfa6acb42175411
phemmz <phemmzmcllroy@gmail.com>
2017-07-21 11:34:37 +0100

    React router and basic navigation between pages

--- client/src/components/layout/Main.js
+++ client/src/components/layout/Main.js
@@ -6,19 +6,68 @@ class Main extends Component {
     render() {
     	return (
     		<div>
-	    		<nav>
-				    <div className="nav-wrapper teal lighten-2">
-					        <Link to="/dashboard" ><em className="post">POSTIT</em></Link>
-					        <ul id="nav-mobile" className="right hide-on-med-and-down">
-						        <li><Link to="/login" className="waves-effect waves-light btn">Login</Link></li>
-						        <li><Link to="/signup"  className="waves-effect waves-light btn">Signup</Link></li>
-					        </ul>
-				    </div>
-				</nav>
+	    		<header>
+					<nav className="black navbar navbar-default" role="navigation">
+						<div className="nav-wrapper">
+							<div className="container-fluid">
+								<div className="row">
+									<div className="col s2" >
+										<Link to="/dashboard" className="brand-logo"><img className="postlogo" src="POSTIT.png" alt="postit logo" /></Link>
+										<a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+									</div>
+									<div className="col s10">
+										<ul className="right hide-on-med-and-down">
+											<li><a href="#modal1" className="waves-effect waves-light btn">Login</a></li>
+											<li><a href="#modal2" className="waves-effect waves-light btn">Sign Up</a></li>
+										</ul>
+										<ul className="side-nav" id="mobile-demo">
+											<li><a href="./login.html" className="waves-effect waves-light btn">Login</a></li>
+											<li><a href="./signup.html" className="waves-effect waves-light btn">Sign Up</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</nav>
+				</header>
+				<div className="row modal" id="modal2">
+					<div className="modal-content">
+						<div className="welc grouplist">
+							<form className="col s12" action="api/user/signup" method="post">
+								<div className= "row">
+									<h3 className="userlogin">SIGN UP</h3>
+								</div>
+								<div className="row">
+									<div className="input-field col s6">
+										<input id="email" type="text" className="validate" />
+										<label for="email">Email</label>
+									</div>
+								</div>
+								<div className="row">
+									<div className="input-field col s6">
+										<input id="username" type="text" className="validate" />
+										<label for="username">Username</label>
+									</div>
+								</div>
+								<div className="row">
+									<div className="input-field col s6">
+										<input id="password" type="password" className="validate" />
+										<label for="password">Password</label>
+									</div>
+								</div>
+								<div className="row">
+									<a className="waves-effect waves-light btn" href="./dashboard.html">Sign Up</a>
+								</div>
+								<div className="modal-footer">
+									<a href="#" className="btn modal-action modal-close waves-effect waves-green btn-flat"Close></a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
 				<div className="container">
 				    {this.props.children}
 				</div>
-
 				<footer className="footer">			          
 			          <div className="footer-copyright teal lighten-2">
 			            <div className="container">
@@ -27,7 +76,7 @@ class Main extends Component {
 			          </div>
 		        </footer>
 			</div>
-        )
+        );
     }
 
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,48 +4,53 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
 
-	entry: [
-		'webpack-hot-middleware/client',
-		path.join(__dirname, '/client/src/app.js')
-	],
-	output: {
-		path: '/',
-		publicPath: '/',
-		filename: 'bundle.js',
-		sourceMapFilename: 'bundle.map'
-	},
-	devtool: '#source-map',
-	module: {
-		loaders: [
-			{
-			    test: /\.js$/,
-			    include: [
-					path.join(__dirname, 'client')
-				],
-				exclude: /node_modules/,
-			    loaders: ['react-hot-loader', 'babel-loader']
-			},
-			{
-				test: /\.scss$/,
-				use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-					use: [
-					    {
-					    	loader: "css-loader"
-					    },
-					    {
-					    	loader: "sass-loader"
-					    }
-					],
-					fallback: "style-loader"
-				}))
-				
-			}
-		]
-
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.optimize.OccurrenceOrderPlugin(),
-	    new ExtractTextPlugin({filename: './styles.css', allChunks: true})
-	],
-}
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/src/app.js')
+  ],
+  output: {
+    path: '/',
+    publicPath: '/',
+    filename: 'bundle.js',
+    sourceMapFilename: 'bundle.map'
+  },
+  devtool: '#source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'client')
+        ],
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader', 'babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ],
+          fallback: 'style-loader'
+        }))
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?name=/[name].[ext]',
+          'image-webpack-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin({ filename: './styles.css', allChunks: true })
+  ],
+};
