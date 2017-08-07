ee6a81c902f9f71bfd64bd2e1a251c3ee94bfab0
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 08:56:31 +0100

    Set authorization token in the header

--- client/src/utils/setAuthorizationToken.js
+++ client/src/utils/setAuthorizationToken.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 /**
- * 
+ * setAuthorizationToken
+ * Sets Authorization on request headers if token is available
+ * Deletes Authorization if token is false
+ * The reason why JWT is used and attached to the header
+ * is to prove that the sent data was actually created by an authentic source.
  * @param {*} token
+ * @returns {*} header
  */
 export default function setAuthorizationToken(token) {
   if (token) {
@@ -9,4 +14,4 @@ export default function setAuthorizationToken(token) {
   } else {
     delete axios.defaults.headers.common['Authorization'];
   }
-} 
+}
