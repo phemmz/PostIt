7c03761e58bbf2341e155119c42757ae48ca5780
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 09:22:34 +0100

    Add jsdoc comments

--- server/controllers/middlewares/signinMiddleware.js
+++ server/controllers/middlewares/signinMiddleware.js
@@ -1,13 +1,14 @@
 import sharedSigninValidations from '../../shared/loginValidations';
 /**
- *
+ * SigninValidations class
  */
 export default class SigninValidations {
 /**
- *
+ * validateUserInput()
  * @param {*} req
  * @param {*} res
  * @param {*} next
+ * @returns {object} json
  */
   static validateUserInput(req, res, next) {
     const { errors, isValid } = sharedSigninValidations.validateSignin(req.body);
