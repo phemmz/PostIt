6918cd8e420658c6ec4d9a0bf16ccc50ba0cf0e6
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 08:50:22 +0100

    Redesign presentational component for creating a message

--- client/src/components/presentation/CreateMessage.js
+++ client/src/components/presentation/CreateMessage.js
@@ -2,52 +2,66 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class CreateMessage extends Component {
-  constructor() {
-		super();
+  constructor(props) {
+		super(props);
 		this.state = {
 			messages: {
 				content: '',
 				groupId: '',
-				readCheck: '',
-				priority: ''
+				readCheck: false,
+				priority: '1'
 			},
-
+     
 			list: []
 		}
+    this.updateMessage = this.updateMessage.bind(this);
 	}
 
-  updateMessage(event) {
+  componentDidMount() {
+    window.$('select').material_select();
+  }
+
+  updateMessage(e) {
 		let updatedMessage = Object.assign({}, this.state.messages);
-		updatedMessage[event.target.id] = event.target.value;
+		updatedMessage[e.target.id] = e.target.value;
+    updatedMessage['groupId'] = this.props.selectedGroup;
 		this.setState({
 			messages: updatedMessage
 		})
 	}
 
-  sendMessage() {
-    console.log('submitMessage: ' + JSON.stringify(this.state.messages));
+  sendMessage(e) {
 		this.props.onCreate(this.state.messages);
 	}
 
   render() {
-    return (
-      <div>
-        <h4 className="green-text text-darken-4">Send Message</h4>					
-        <div className="container">
-          <div className="row">
-            <div className="input-field col s8">
-              <input onChange={this.updateMessage.bind(this)} id="priority" type="text" className="form-control" />
-              <label htmlFor="priority">Priority</label>																
+    let content = null;
+    if (this.props.selectedGroup) {
+      content = 
+        <div className="comment valign-wrapper center-align">
+          <div id="message-input" className="col l12">
+            <div className="row">
+              <div className="input-field col s5">
+                <select id="priority" value={this.state.messages.priority} onChange={this.updateMessage} style={{display: 'block'}}>
+                  <option value="" disabled>Select Priority</option>
+                  <option value="1">Normal</option>
+                  <option value="2">Urgent</option>
+                  <option value="3">Critical</option>
+                </select>
+              </div>
             </div>
-            <div className="input-field col s8">
-              <input onChange={this.updateMessage.bind(this)} id="content" type="text" className="form-control" />
-              <label htmlFor="content">Message</label>
-              <button onClick={this.sendMessage.bind(this)} className="waves-effect waves-light btn">Send</button>								
+            <div className="row">
+              <div className="col s10">
+                <textarea onChange={this.updateMessage} id="content" className="validate" defaultValue={""} />
+              </div>
+              <div className="col s2">
+                <a onClick={this.sendMessage.bind(this)} className="btn-floating green"><i className="large material-icons">chat</i></a>
+              </div>
             </div>
-          </div>										
+          </div>
         </div>
-      </div>
-    )
+    } 
+    return content;
   }
 }
 
