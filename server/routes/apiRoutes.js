fa5825e647e931934faff68915f2cb81a9032881
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 09:27:15 +0100

    Add jsdoc comments

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -10,16 +10,57 @@ import checkUserInGroup from '../controllers/middlewares/checkUserInGroup';
 import SendMessageValidations from '../controllers/middlewares/sendMessageMiddleware';
 import authenticate from '../controllers/middlewares/authenticate';
 
+/**
+ * Creates a new express router
+ */
 const router = express.Router();
-
+/**
+ * Router for getting all the registered users in the application
+ */
 router.get('/api/user', UserController.getAllUsers);
+/**
+ * Router for identifying a particular user either by username or email
+ */
 router.get('/api/user/:identifier', UserController.getOne);
+/**
+ * Router for signup
+ * Takes a middleware that validates user input
+ */
 router.post('/api/user/signup', SignupValidations.validateUserInput, UserController.signup);
+/**
+ * Router for signin
+ * Takes a middleware that validates user input
+ */
 router.post('/api/user/signin', SigninValidations.validateUserInput, UserController.signin);
+/**
+ * Post router for creating a group
+ * Takes authenticate and validateUserInput middlewares
+ * authenticate verifies the jwtoken and
+ * sets the user details to currentUser if authentication is valid
+ */
 router.post('/api/group', authenticate, CreateGroupValidations.validateUserInput, GroupController.createGroup);
-router.get('/api/group', GroupController.getGroup);
-router.post('/api/group/:groupId/message', SendMessageValidations.validateUserInput, checkUserInGroup.isGroupMember, MessageController.sendMessage);
-router.get('/api/group/:groupId/messages', checkUserInGroup.isGroupMember, MessageController.getMessages);
-router.post('/api/group/:groupId/user', AddUserValidations.validateUserInput, checkUserInGroup.isGroupMember, GroupController.addUserToGroup);
+/**
+ * Router to get all the group(s) a user belongs to
+ * Takes authenticate middleware to verify the token
+ */
+router.get('/api/group', authenticate, GroupController.getGroup);
+/**
+ * Router to post message to a particular group
+ * Takes authenticate, validateUserInput and isGroupMember middlewares
+ * authenticate verifies token
+ * validateUserInput validates user input
+ * isGroupMember checks if the user belongs to the group
+ */
+router.post('/api/group/:groupId/message', authenticate, SendMessageValidations.validateUserInput, checkUserInGroup.isGroupMember, MessageController.sendMessage);
+/**
+ * Router to get all messages in a particular group
+ * Takes the authenticate and isGroupMember middlewares
+ */
+router.get('/api/group/:groupId/messages', authenticate, checkUserInGroup.isGroupMember, MessageController.getMessages);
+/**
+ * Router to add a user to a particular group
+ * Takes the authenticate, validateUserInput and isGroupMember middlewares
+ */
+router.post('/api/group/:groupId/user', authenticate, AddUserValidations.validateUserInput, checkUserInGroup.isGroupMember, GroupController.addUserToGroup);
 
 export default router;
