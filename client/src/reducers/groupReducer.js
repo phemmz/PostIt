d4913caeca97ef8493d5ba764636500b50096751
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 08:55:04 +0100

    Create reducer for group  actions

--- client/src/reducers/groupReducer.js
+++ client/src/reducers/groupReducer.js
@@ -1,4 +1,4 @@
-import { GROUPS_RECEIVED, GROUP_CREATE, GROUP_SELECTED, APPLICATION_STATE, GROUP_MESSAGES } from '../actions/types';
+import { GROUPS_RECEIVED, GROUPS_NOT_RECEIVED, GROUP_CREATE, GROUP_SELECTED, APPLICATION_STATE, GROUP_MESSAGES, ADD_USER } from '../actions/types';
 
 const initialState = {
   groupList: [],
@@ -6,13 +6,23 @@ const initialState = {
   groupMessages: [],
   appStatus: 'ready'
 };
-
+/**
+ * Group reducer takes the state and action and then returns the state
+ * with groupList, selectedGroup, groupMessages and appStatus
+ * @param {*} state
+ * @param {*} action
+ * @returns {object} state
+ */
 export default (state = initialState, action = {}) => {
   const updated = Object.assign({}, state);
   switch (action.type) {
     case GROUPS_RECEIVED:
       updated['groupList'] = action.groups;
       updated['appStatus'] = 'ready';
+      return updated;
+
+    case GROUPS_NOT_RECEIVED:
+      updated['appStatus'] = 'no groups';
       return updated;
 
     case GROUP_CREATE:
@@ -31,6 +41,10 @@ export default (state = initialState, action = {}) => {
 
     case GROUP_MESSAGES:
       updated['groupMessages'] = action.groups;
+      return updated;
+
+    case ADD_USER:
+      updated['addUser'] = action.addUser;
       return updated;
 
     default:
