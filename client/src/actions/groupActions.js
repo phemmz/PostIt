9254574c75624f22e82267fe52ad046c4a8b3044
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 01:36:28 +0100

    Add jsdoc comments and refactor code to use javascript class

--- client/src/actions/groupActions.js
+++ client/src/actions/groupActions.js
@@ -1,68 +1,117 @@
 import axios from 'axios';
-import { GROUP_CREATE, GROUPS_RECEIVED, GROUP_SELECTED, CURRENT_USER_RECEIVED, USER_RECEIVED, APPLICATION_STATE, GROUP_MESSAGES } from './types';
-
-export function groupsReceived(groups) {
-  return {
-    type: GROUPS_RECEIVED,
-    groups
+import { GROUP_CREATE, GROUPS_RECEIVED, GROUPS_NOT_RECEIVED, GROUP_SELECTED, CURRENT_USER_RECEIVED, USER_RECEIVED, APPLICATION_STATE, GROUP_MESSAGES, ADD_USER } from './types';
+/**
+ * GroupActions class
+ */
+export default class GroupActions {
+  /**
+   * groupsRecieved()
+   * @param {object} groups
+   * @returns {object} type, groups
+   */
+  static groupsReceived(groups) {
+    return {
+      type: GROUPS_RECEIVED,
+      groups
+    };
   }
-}
-
-export function groupCreate(group) {
-  return {
-    type: GROUP_CREATE,
-    group
-  };
-}
-
-export function groupSelected(groupId) {
-  return {
-    type: GROUP_SELECTED,
-    selectedGroup: groupId
+  /**
+   * groupCreate()
+   * @param {object} group
+   * @returns {object} type, group
+   */
+  static groupCreate(group) {
+    return {
+      type: GROUP_CREATE,
+      group
+    };
   }
-}
-
-export function currentUserReceived(user) {
-  return {
-    type: CURRENT_USER_RECEIVED,
-    user
+  /**
+   * groupSelected action saves the groupId of the group clicked on to the state
+   * @param {integer} groupId
+   * @returns {object} type, selectedGroup
+   */
+  static groupSelected(groupId) {
+    return {
+      type: GROUP_SELECTED,
+      selectedGroup: groupId
+    };
   }
-}
-
-export function userReceived(user) {
-  return {
-    type: USER_RECEIVED,
-    user
+  /**
+   * @param {object} user
+   * @return {object} type,user
+   */
+  static currentUserReceived(user) {
+    return {
+      type: CURRENT_USER_RECEIVED,
+      user
+    };
   }
-}
-
-export function fetchGroups(params) {
-  return (dispatch) => {
-    dispatch({
-      type: APPLICATION_STATE,
-      status: 'loading'
-    })
-
-    axios.get('/api/group', params)
-      .then((response) => {
-        const groups = response.data.results;
-        dispatch({
-          type: GROUPS_RECEIVED,
-          groups
-        })
-      })
+  /**
+   * @param {object} user
+   * @return {object} user
+   */
+  static userReceived(user) {
+    return {
+      type: USER_RECEIVED,
+      user
+    };
   }
-}
-export function groupMessages(groupId) {
-  return (dispatch) => {
-    axios.get(`api/group/${groupId}/messages`)
-      .then((response) => {
-        const groups = response.data.results;
-        console.log(groups, 'group messgaes');
-        dispatch({
-          type: GROUP_MESSAGES,
-          groups
-        });
+  /**
+   * @param {*} params
+   * @returns {object} type,status,groups
+   */
+  static fetchGroups(params) {
+    return (dispatch) => {
+      dispatch({
+        type: APPLICATION_STATE,
+        status: 'loading'
       });
-  };
+      axios.get('/api/group', params)
+        .then((response) => {
+          const groups = response.data.results;
+          dispatch({
+            type: GROUPS_RECEIVED,
+            groups
+          });
+        })
+        .catch(() => {
+          dispatch({
+            type: GROUPS_NOT_RECEIVED
+          });
+        });
+    };
+  }
+  /**
+   * @param {integer} groupId
+   * @returns {object} type, groups
+   */
+  static groupMessages(groupId) {
+    return (dispatch) => {
+      axios.get(`api/group/${groupId}/messages`)
+        .then((response) => {
+          const groups = response.data.results;
+          dispatch({
+            type: GROUP_MESSAGES,
+            groups
+          });
+        });
+    };
+  }
+  /**
+   * @param {integer} groupId
+   * @param {object} userAdded
+   * @returns {object} type, addUser
+   */
+  static addUser(groupId, userAdded) {
+    return (dispatch) => {
+      axios.post(`api/group/${groupId}/user`, userAdded)
+        .then((response) => {
+          dispatch({
+            type: ADD_USER,
+            addUser: response.data.results
+          });
+        });
+    };
+  }
 }
