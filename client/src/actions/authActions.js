393ba47659596bda00c14a921c480ff83acad238
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 01:19:31 +0100

    Add jsdoc comments and refactor code to use javascript class

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,29 +2,58 @@ import jwtDecode from 'jwt-decode';
 import axios from 'axios';
 import setAuthorizationToken from '../utils/setAuthorizationToken';
 import { SET_CURRENT_USER } from './types';
-
-export function setCurrentUser(user) {
-  return {
-    type: SET_CURRENT_USER,
-    user
-  };
+/**
+ * AuthenticationActions class
+ */
+export default class AuthenticationActions {
+  /**
+   * The setCurrentUser action gets called from the
+   * login action and it takes in the decoded token
+   * @param {object} user
+   * @returns {object} type, user
+   */
+  static setCurrentUser(user) {
+    return {
+      type: SET_CURRENT_USER,
+      user
+    };
+  }
+  /**
+   * logout signs the user out and removes the token from localstorage
+   * @returns {*} dispatch
+   */
+  static logout() {
+    return (dispatch) => {
+      /**
+       * this removes the token in the localstorage
+       */
+      localStorage.removeItem('jwtToken');
+      setAuthorizationToken(false);
+      dispatch(AuthenticationActions.setCurrentUser({}));
+    };
+  }
+  /**
+   * The login action takes the form data and calls axios.
+   * It Gets the token from the server and saves it in localstorage
+   * It also sets the header with the token
+   * @param {object} data
+   * @returns {*} axios
+   */
+  static login(data) {
+    return (dispatch) => {
+      return axios.post('api/user/signin', data).then((res) => {
+        const token = res.data.token;
+        /**
+         * this saves the token in the localstorage as a key value object
+         */
+        localStorage.setItem('jwtToken', token);
+        setAuthorizationToken(token);
+        /**
+         * the jwtDecode is a small browser library that helps
+         * decoding JWTs token which are Base64Url encoded
+         */
+        dispatch(AuthenticationActions.setCurrentUser(jwtDecode(token)));
+      });
+    };
+  }
 }
-
-export function logout() {
-  return (dispatch) => {
-    localStorage.removeItem('jwtToken');
-    setAuthorizationToken(false);
-    dispatch(setCurrentUser({}));
-  };
-}
-
-export function login(data) {
-  return (dispatch) => {
-    return axios.post('api/user/signin', data).then((res) => {
-      const token = res.data.token;
-      localStorage.setItem('jwtToken', token);
-      setAuthorizationToken(token);
-      dispatch(setCurrentUser(jwtDecode(token)));
-    });
-  };
-} 
