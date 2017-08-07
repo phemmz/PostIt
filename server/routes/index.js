81640b3ab53fc267ddacc7141fe54dac75708f73
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 09:27:32 +0100

    Remove index file

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-
-// const router = express.Router();
-
-export default (app) => {
-  app.get('*', (req, res) => {
-    res.render('index', { title: 'PostIt' });
-  });
-};
