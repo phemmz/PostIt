c096bbfb32e0e1d6b2326cb69a9e289cee1c9562
phemmz <phemmzmcllroy@gmail.com>
2017-08-07 08:53:42 +0100

    Create reducer for authentication action

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,11 +1,18 @@
 import isEmpty from 'lodash/isEmpty';
 import { SET_CURRENT_USER } from '../actions/types';
-
+/**
+ * Initial state
+ */
 const initialState = {
   isAuthenticated: false,
   user: {}
 };
-
+/**
+ * Auth reducer takes the state and action and then returns the state
+ * with isAuthenticated and the decoded token
+ * @param {*} state
+ * @param {*} action
+ */
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case SET_CURRENT_USER:
@@ -15,4 +22,4 @@ export default (state = initialState, action = {}) => {
       };
     default: return state;
   }
-}
+};
