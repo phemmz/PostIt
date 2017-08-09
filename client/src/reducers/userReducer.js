659cd42586def16107de6d7fe4e07fb4b2001c4d
phemmz <phemmzmcllroy@gmail.com>
2017-08-09 14:40:34 +0100

    Add reducer for getting users

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,13 +1,18 @@
-import { USER_RECEIVED } from '../actions/types';
+import { GET_ALL_USERS, USER_RECEIVED } from '../actions/types';
 
 const initialState = {
-  usersMap: {}
+  usersMap: {},
+  users: []
 };
 
 export default (state = initialState, action = {}) => {
   const updated = Object.assign({}, state);
   const updatedUsersMap = Object.assign([], updated.usersMap);
   switch (action.type) {
+    case GET_ALL_USERS:
+      updated['users'] = action.users;
+      return updated;
+
     case USER_RECEIVED:
       updatedUsersMap[action.user.username] = action.user;
       updated.usersMap = updatedUsersMap;
