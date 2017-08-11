4dd3843debda4ca386ca1c7d4e70bb9ac0e22cec
phemmz <phemmzmcllroy@gmail.com>
2017-08-11 15:49:21 +0100

    Add sidenav and usermodal to index export

--- client/src/components/presentation/index.js
+++ client/src/components/presentation/index.js
@@ -2,10 +2,14 @@ import CreateMessage from './CreateMessage';
 import Group from './Group';
 import Message from './Message';
 import CreateGroup from './CreateGroup';
+import AddUserModal from './AddUserModal';
+import SideNav from './SideNav';
 
 export {
   CreateMessage,
   Group,
   Message,
-  CreateGroup
+  CreateGroup,
+  AddUserModal,
+  SideNav
 };
