0cc16bb1a5aea04a187ab1f4ae1afe3908f0ca4e
phemmz <phemmzmcllroy@gmail.com>
2017-08-17 13:55:26 +0100

    Remove flasmessage

--- client/src/components/layout/SignupPage.js
+++ client/src/components/layout/SignupPage.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import SignupActions from '../../actions/signupActions';
-import { addFlashMessage } from '../../actions/flashMessages';
 import SignupForm from './signup/SignupForm';
 /**
  * SignupPage class
  */
 class SignupPage extends Component {
 	render() {
-		const { userSignupRequest, addFlashMessage, isUserExists } = this.props;
+		const { userSignupRequest, isUserExists } = this.props;
 		return (
 			<div className="container">
 				<SignupForm
 					isUserExists={isUserExists}
 					userSignupRequest={userSignupRequest}
-					addFlashMessage={addFlashMessage} 
 				/>
 			</div>
 		)
@@ -25,8 +23,7 @@ class SignupPage extends Component {
 const dispatchToProps = (dispatch) => {
   return {
     userSignupRequest: (userData) => dispatch(SignupActions.userSignupRequest(userData)),
-    isUserExists: (identifier) => dispatch(SignupActions.isUserExists(identifier)),
-    addFlashMessage: (message) => dispatch(addFlashMessage(message))
+    isUserExists: (identifier) => dispatch(SignupActions.isUserExists(identifier))
   }
 }
 
