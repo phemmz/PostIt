1c9821f33c9c7a02785bd06e14104da7e353b92d
phemmz <phemmzmcllroy@gmail.com>
2017-08-20 12:25:35 +0100

    Fix webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-import path from 'path';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 
