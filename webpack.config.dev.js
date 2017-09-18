a16b17929a2bf6fac66878bb365f112c37944529
phemmz <phemmzmcllroy@gmail.com>
2017-09-18 04:41:16 +0100

    chore: setup production webpack - setup webpack for production build of react [Finishes #151160877]

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 module.exports = {
 
